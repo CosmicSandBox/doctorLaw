a143f4af37ee8dc614dcf06bde8d38ce5e932632
Kimjaewoo <kimjaewoo@hufs.ac.kr>
2024-01-16 13:18:33 +0900

    [ADD] 마일리지 충전 페이지

--- src/pages/MileageShop/MileageCharge/FreeMileage.jsx
+++ src/pages/MileageShop/MileageCharge/FreeMileage.jsx
@@ -1,5 +1,5 @@
-import styled from 'styled-components';
-import ContainerColumn from '../../../Components/Container/ContainerColumn';
+import styled from "styled-components";
+import ContainerColumn from "../../../Components/Container/ContainerColumn";
 
 const FreeTypeContainer = styled.div`
     display: flex;
@@ -11,84 +11,84 @@ const FreeTypeContainer = styled.div`
     margin: 15px 0;
     border-radius: 15px;
 
-    background: ${props => props.$background};
-`
+    background: ${(props) => props.$background};
+`;
 const FreeTypeHeader = styled(ContainerColumn)`
     gap: 1rem;
-`
-const FreeMileageContainer = styled(ContainerColumn)`
-
-`
+`;
+const FreeMileageContainer = styled(ContainerColumn)``;
 
 const TextHeader = styled.h1`
     font-family: LINE-Bd;
-    font-size: 2rem;
-`
+    //font-size: 2rem;
+    font-size: 25px;
+    padding : 10px;
+`;
 const TextFreeHeader = styled.h1`
     font-family: LINE-Bd;
     font-size: 1.5rem;
-`
+`;
 const TextFreeDescription = styled.h1`
     font-family: LINE-Rg;
     font-size: 1rem;
-    color: ${props => props.$color || "gray"};
-`
+    color: ${(props) => props.$color || "gray"};
+`;
 
 const NavButton = styled.button`
     display: flex;
     flex-direction: row;
     align-items: center;
-    width: 250px;
-    height: 70px;
+    width: 300px;
+    height: 90px;
 
     border: none;
     border-radius: 15px;
 
-    background: ${props => props.$background};
-`
+    background: ${(props) => props.$background};
+`;
 const ButtonImg = styled.img`
-    flex:3;
+    flex: 3;
     object-fit: cover;
     width: 70px;
-`
+`;
 const ButtonTextContainer = styled.div`
-    flex:7;
+    flex: 7;
     color: white;
-`
-export default function Comopnent(){
-    return(
+`;
+export default function Comopnent() {
+    return (
         <FreeMileageContainer>
             <TextHeader>무료 마일리지 받기</TextHeader>
 
             <FreeTypeContainer $background="#D6E4F7">
                 <FreeTypeHeader>
                     <TextFreeHeader>광고 보고 무료 마일리지 받기</TextFreeHeader>
-                    <TextFreeDescription> 설명 </TextFreeDescription>
+                    <TextFreeDescription> 30초 동영상 광고를 시청하고 1000 마일리지를 받으세요! </TextFreeDescription>
                 </FreeTypeHeader>
 
                 <NavButton $background="#122033">
-                    <ButtonImg src="/img/coins.png"/>
+                    <ButtonImg src="/img/charge/동영상아이콘 1.svg" />
                     <ButtonTextContainer>
                         <TextFreeHeader>광고 시청</TextFreeHeader>
-                        <TextFreeDescription $color="white"> 설명 </TextFreeDescription>
+                        <TextFreeDescription $color="white"> 1000 마일리지 적립 </TextFreeDescription>
                     </ButtonTextContainer>
                 </NavButton>
             </FreeTypeContainer>
 
-            <FreeTypeContainer $background="#D6E4F7">
+            <FreeTypeContainer $background="#FFE1D3">
                 <FreeTypeHeader>
-                    <TextFreeHeader>광고 보고 어쩌구</TextFreeHeader>
-                    <TextFreeDescription> 귀찮다 알아서 해줘 </TextFreeDescription>
+                    <TextFreeHeader>공유하고 무료 마일리지 받기</TextFreeHeader>
+                    <TextFreeDescription> 법학박사를 친구들에게 공유하고 500 마일리지를 받으세요! </TextFreeDescription>
                 </FreeTypeHeader>
 
-                <NavButton $background="orange">
-                    <ButtonImg src="/img/coins.png"/>
+                <NavButton $background="#FF7B3F">
+                    <ButtonImg src="/img/charge/공유아이콘 1.svg" />
                     <ButtonTextContainer>
-                        <TextFreeHeader>공유하래</TextFreeHeader>
-                        <TextFreeDescription $color="white"> 대충 오렌지 색 </TextFreeDescription>
+                        <TextFreeHeader>공유하기</TextFreeHeader>
+                        <TextFreeDescription $color="white"> 500 마일리지 적립 </TextFreeDescription>
                     </ButtonTextContainer>
                 </NavButton>
             </FreeTypeContainer>
         </FreeMileageContainer>
-    )
+    );
 }
--- src/pages/MileageShop/MileageCharge/HowToPay.jsx
+++ src/pages/MileageShop/MileageCharge/HowToPay.jsx
@@ -8,7 +8,9 @@ const FreeMileageContainer = styled(ContainerColumn)`
 
 const TextHeader = styled.h1`
     font-family: LINE-Bd;
-    font-size: 2rem;
+    //font-size: 2rem;
+    font-size: 25px;
+    padding: 10px;
 `
 
 const GridContainer = styled.div`
@@ -24,7 +26,7 @@ function GridButton({$img, children}){
         display: flex;
         flex-direction: column;
         align-items: center;
-        padding: 10px;
+        padding: 40px;
         border: none;
         border-radius: 5px;
         background-color:
@@ -36,11 +38,13 @@ function GridButton({$img, children}){
     `;
     const Text = styled.p`
         font-family: LINE-Rg;
-        font-size: 1rem;
+        font-size: 2rem;
     `
     const Image = styled.img`
         height: 50px;
     `
+
+
     return(
         <Button>
             <Image src={$img}></Image>
@@ -52,14 +56,14 @@ function GridButton({$img, children}){
 export default function Comopnent(){
     return(
         <FreeMileageContainer>
-            <TextHeader>무료 마일리지 받기</TextHeader>
+            <TextHeader>결제 수단 선택하기</TextHeader>
             <GridContainer>
-                <GridButton $img="/img/coins.png">신용카드</GridButton>
-                <GridButton $img="/img/coins.png">휴대폰</GridButton>
-                <GridButton $img="/img/coins.png">무통장입금</GridButton>
-                <GridButton $img="/img/coins.png">toss pay</GridButton>
-                <GridButton $img="/img/coins.png">N pay</GridButton>
-                <GridButton $img="/img/coins.png">카카오페이</GridButton>
+                <GridButton $img="/img/charge/신용카드.png">신용카드</GridButton>
+                <GridButton $img="/img/charge/휴대폰.png">휴대폰</GridButton>
+                <GridButton $img="/img/charge/무통장입금.png" >무통장입금</GridButton>
+                <GridButton $img="/img/charge/toss_logo.svg">toss pay</GridButton>
+                <GridButton $img="/img/charge/네이버페이2.png">네이버페이</GridButton>
+                <GridButton $img="/img/charge/카카오페이!.png" >카카오페이</GridButton>
             </GridContainer>
             
         </FreeMileageContainer>
